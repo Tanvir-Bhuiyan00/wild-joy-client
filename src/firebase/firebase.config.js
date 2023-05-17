996c9d16bfec2277d513ee70dce8ab9b535865a8
Tanvir-Bhuiyan00 <mdtanvir95925@gmail.com>
2023-05-17 23:40:50 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdPUs_lHpq1TK7D1CCPYFEn3-VBkywBbc",
+  authDomain: "wild-joy-d8303.firebaseapp.com",
+  projectId: "wild-joy-d8303",
+  storageBucket: "wild-joy-d8303.appspot.com",
+  messagingSenderId: "160545970302",
+  appId: "1:160545970302:web:a9491d47b0c780129b202f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
