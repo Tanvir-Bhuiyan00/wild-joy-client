d0100032fa6ab03e05db10c5c24b8e3dc393102a
Tanvir-Bhuiyan00 <mdtanvir95925@gmail.com>
2023-05-18 20:55:45 +0600

    register page added

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,8 +1,7 @@
 import { Link } from "react-router-dom";
 import loginBanner from "../../assets/login-banner.png";
-import SocialLogin from "./SocialLogin";
 
-const LogIn = () => {
+const Register = () => {
   return (
     <div className="my-10">
       <div className="hero min-h-screen bg-base-200">
@@ -13,15 +12,26 @@ const LogIn = () => {
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <div className="card-body">
               <h2 className="font-display text-4xl text-wildJoyColorThree mb-5 font-bold text-center">
-                Please Login
+                Please Register
               </h2>
               <form>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-bold">Your Name</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="name"
+                    className="input  border-wildJoyColorTwo rounded-md input-bordered"
+                  />
+                </div>
                 <div className="form-control">
                   <label className="label">
                     <span className="label-text font-bold">Email</span>
                   </label>
                   <input
-                    type="text"
+                    type="email"
                     name="email"
                     placeholder="email"
                     className="input  border-wildJoyColorTwo rounded-md input-bordered"
@@ -32,9 +42,20 @@ const LogIn = () => {
                     <span className="label-text font-bold">Password</span>
                   </label>
                   <input
-                    type="text"
+                    type="password"
                     name="password"
                     placeholder="password"
+                    className="input border-wildJoyColorTwo rounded-md input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text font-bold">Photo URL</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="photo"
+                    placeholder="url"
                     className="input border-wildJoyColorTwo rounded-md input-bordered"
                   />
                 </div>
@@ -42,17 +63,17 @@ const LogIn = () => {
                   <input
                     className=" btn hover:bg-wildJoyColorTwo hover:text-wildJoyColorOne"
                     type="submit"
-                    value="Login"
+                    value="Register"
                   />
                 </div>
               </form>
               <p className="my-4 text-center text-wildJoyColorTwo font-bold">
-                New To Wild Joy{" "}
-                <Link className="text-wildJoyColorThree" to="/register">
-                  Register
+                Already Have An Account{" "}
+                <Link className="text-wildJoyColorThree" to="/login">
+                  Login
                 </Link>
               </p>
-              <SocialLogin></SocialLogin>
+              
             </div>
           </div>
         </div>
@@ -61,4 +82,4 @@ const LogIn = () => {
   );
 };
 
-export default LogIn;
+export default Register;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,24 +2,27 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
 import Home from "../Pages/Home/Home/Home";
 import LogIn from "../Pages/LogIn/LogIn";
-
+import Register from "../Pages/Register/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
-    children:[
+    children: [
       {
         path: "/",
-        element: <Home></Home>
+        element: <Home></Home>,
       },
       {
         path: "login",
-        element: <LogIn></LogIn>
-      }
-    ]
+        element: <LogIn></LogIn>,
+      },
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
-
 
 export default router;
